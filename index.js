534a56f0513f55abe6094b716c9b0aba7381ada9
HugoStewart06 <hugoportier3@gmail.com>
2021-01-13 16:11:02 +0100

    commit

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 const express = require('express')
-const mysql = require('./db')
-const { SERVER_PORT } = require('./env')
+const cors = require('cors')
+const connection = require("./config");
 
+const port = 5000;
 const app = express();
+app.use(cors())
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }))
+connection.connect(function (err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+  
+  console.log('connected as id ' + connection.threadId);
+});
 
-require('./routes')(app)
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.status(200).json('stay a while listen...');
 });
 
-app.listen(SERVER_PORT, () => {
-    console.log(`server is listening on ${SERVER_PORT}`);
+app.listen(port, () => {
+    console.log(`server is listening on ${port}`);
 });
