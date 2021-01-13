98a7ea0d08d25eac4ca3781416514bb6da11f99d
HugoStewart06 <hugoportier3@gmail.com>
2021-01-13 15:26:59 +0100

    structuration classique

--- routes/index.js
+++
@@ -1,5 +0,0 @@
-const userRoutes = require('./users');
-
-module.exports = (app) => {
-    app.use('/users', userRoutes)
-}
--- routes/users.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const usersController = require('../controllers/usersController');
-
-router.get('/', usersController.getAll);
-router.get('/:id', usersController.getOne);
-router.post('/', usersController.handlePost);
-
-module.exports = router
